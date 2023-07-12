a98d3c25ec03a2799c44cbb348dd04f47053326c
Ethan Phillips <ethansemail@yahoo.com>
2023-07-12 15:37:07 -0500

    Initial Commit

---
+++ Grind Tasks/Grind_TasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grind_TasksApp.swift
+//  Grind Tasks
+//
+//  Created by Ethan Phillips on 7/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Grind_TasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
