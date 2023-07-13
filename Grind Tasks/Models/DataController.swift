68e6e7802111d98424b1a6c058d9991d46ef9b28
Ethan Phillips <ethansemail@yahoo.com>
2023-07-13 10:56:45 -0500

    Core Data Setup - Added Data controller to handle the data and store in long term storage. - Added CloudKit functionality so you can sync tasks across devices.

---
+++ Grind Tasks/Models/DataController.swift
@@ -0,0 +1,88 @@
+//
+//  DataController.swift
+//  Grind Tasks
+//
+//  Created by Ethan Phillips on 7/12/23.
+//
+
+import Foundation
+import CoreData
+
+class DataController: ObservableObject {
+    let container: NSPersistentCloudKitContainer
+    
+    static var preview: DataController = {
+        let dataController = DataController(inMemory: true)
+        dataController.createSampleData()
+        return dataController
+    }()
+    
+    init(inMemory: Bool = false) {
+        container = NSPersistentCloudKitContainer(name: "Main")
+        
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(filePath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { storeDescription, error in
+            if let error {
+                fatalError("Fatal error loading store: \(error.localizedDescription)")
+            }
+            
+        }
+    }
+    
+    func createSampleData() {
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            let tag = Tag(context: viewContext)
+            tag.id = UUID()
+            tag.name = "Tag \(i)"
+            
+            for j in 1...10 {
+                let task = TaskItem(context: viewContext)
+                task.title = "Task \(i) - \(j)"
+                task.content = "Description for tasks goes here!"
+                task.creationDate = .now
+                task.completed = Bool.random()
+                task.scheduleTime = Bool.random()
+                tag.addToTasks(task)
+            }
+        }
+        try? viewContext.save()
+    }
+    
+    func save() {
+        if container.viewContext.hasChanges {
+            try? container.viewContext.save()
+        }
+    }
+    
+    func delete(_ object: NSManagedObject) {
+        objectWillChange.send()
+        container.viewContext.delete(object)
+        save()
+    }
+    
+    private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>) {
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        batchDeleteRequest.resultType = .resultTypeObjectIDs
+
+        if let delete = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult {
+            let changes = [NSDeletedObjectsKey: delete.result as? [NSManagedObjectID] ?? []]
+            NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [container.viewContext])
+        }
+    }
+    
+    func deleteAll() {
+        let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
+        delete(request1)
+
+        let request2: NSFetchRequest<NSFetchRequestResult> = TaskItem.fetchRequest()
+        delete(request2)
+
+        save()
+    }
+}
